7dd75f985ffa54dc28bbd8e5fc9c35963b3d06ba
z00528jp <z00528jp@INNM5CD4407MN7.fritz.box>
2025-11-11 13:43:23 +0100

    Programmanpassungen in P3_main

--- src/p3/P3_main.java
+++ src/p3/P3_main.java
@@ -5,14 +5,14 @@ public class P3_main {
 	public static void main(String[] args) {
         boolean[] values = {true, false};
 
-        System.out.printf("%-6s %-6s %-20s %-12s %-12s %-20s%n", "x", "y", "!(x!=y && x==y)", "x || true", "x && false", "(x&&!y)||(!x)");
+        System.out.printf("%-8s %-8s %-22s %-14s %-14s %-22s%n", "x", "y", "!(x!=y && x==y)", "x || true", "x && false", "(x&&!y)||(!x)");
         for (boolean x : values) {
             for (boolean y : values) {
                 boolean a = !(x != y && x == y);
                 boolean b = x || true;
                 boolean c = x && false;
                 boolean d = (x && !y) || (!x);
-                System.out.printf("%-6s %-6s %-20s %-12s %-12s %-20s%n", x, y, a, b, c, d);
+                System.out.printf("%-8s %-8s %-22s %-14s %-14s %-22s%n", x, y, a, b, c, d);
             }
         }
 	}
